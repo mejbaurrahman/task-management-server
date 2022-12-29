6dcd57a8fd1becc1dd1fae47d2068fa5ef95812d
unknown <mejbacsedu@gmail.com>
2022-12-29 21:54:02 +0600

    update-final

--- index.js
+++ index.js
@@ -12,3 +12,76 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvker.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        console.log('Connected Succesfully');
+        const tasks = client.db('taskManagement').collection('tasks');
+        
+        
+        app.post('/tasks', async(req, res)=>{
+            const task = req.body;
+            const result =await tasks.insertOne(task);
+
+            res.send(result);
+        })
+        app.put('/tasks/:id', async(req, res)=>{
+           
+            const id = req.params.id;
+            const query = {
+                _id:ObjectId(id)
+            };
+            const updatedDoc = {
+                $set:{
+                    status: 'complete'
+                }
+            }
+            const result = await tasks.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+        app.put('/completetasks/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {
+                _id:ObjectId(id)
+            };
+            const updatedDoc = {
+                $set:{
+                    status: 'not complete'
+                }
+            }
+            const result = await tasks.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+        app.get('/tasks', async(req, res)=>{
+            const email = req.query.email;
+            const query = {
+                email: email,
+                status: "not complete"
+            }
+            const result =await tasks.find(query).toArray();
+            res.send(result);
+        })
+        app.get('/completetasks', async(req, res)=>{
+            const email = req.query.email;
+            const query = {
+                email: email,
+                status: "complete"
+            }
+            const result =await tasks.find(query).toArray();
+            res.send(result);
+        })
+     
+    }finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+app.get('/', (req, res)=>{
+    res.send('Welcome to Task Management');
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Listening port`, port);
+})
